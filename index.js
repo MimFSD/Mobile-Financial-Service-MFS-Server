32ca4cd6ed1bc9560760073631926df0e4f0623a
MimFSD <mimislam878@gmail.com>
2024-07-16 00:06:06 +0600

    initialize project

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const cors = require('cors')
+
+
+// const jwt = require('jsonwebtoken')
+
+const port = process.env.PORT || 5000
+
+// middleware
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kowhoxx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+//1mphrO6icxpK6C3v
+//mobileSevice
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello from mobile financial service..')
+  })
+  
+  app.listen(port, () => {
+    console.log(`mobile financial service is running on port ${port}`)
+  })
+  
