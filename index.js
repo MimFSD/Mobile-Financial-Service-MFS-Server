df8e2040826d531a75cfd7e05deaa53e3ef83754
MimFSD <mimislam878@gmail.com>
2024-07-30 13:06:41 +0600

    Update

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use(express.json());
 // MongoDB Conection Method
 const uri = `mongodb+srv://${process.env.ENV_READYPAY_USER}:${process.env.ENV_READYPAY_PASSWORD}@cluster0.6e55rfm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
